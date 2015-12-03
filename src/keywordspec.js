d683dcc6d2dbf0929414b9e19ea8ad638eb40535
smhg <sam@strictlyphp.com>
2015-12-03 15:33:49 +0100

    parse multi-keyword string (fix #40)

--- src/keywordspec.js
+++ src/keywordspec.js
@@ -1,16 +1,20 @@
-var specPattern = /([^:]+)(?::(\d)(?:,(\d))?)?/;
+var specPattern = / *([^:,]+) *(?::([^, ]+)(?:,([^, ]+))?)? */g,
+  onlyDigits = /^\d+$/;
 
 function indexify (idx) {
   return parseInt(idx, 10) - 1;
 }
 
 function addToSpec (spec, keyword) {
-  var parts = keyword.match(specPattern);
+  var parts;
 
-  spec[parts[1]] = [parts[2] ? indexify(parts[2]) : 0];
+  while ((parts = specPattern.exec(keyword)) !== null) {
 
-  if (parts[3]) {
-    spec[parts[1]].push(indexify(parts[3]));
+    spec[parts[1]] = [onlyDigits.test(parts[2]) ? indexify(parts[2]) : 0];
+
+    if (parts[3] && onlyDigits.test(parts[3])) {
+      spec[parts[1]].push(indexify(parts[3]));
+    }
   }
 
   return spec;
--- test/keywordspec.js
+++ test/keywordspec.js
@@ -17,9 +17,18 @@ describe('Keywordspec', function () {
   });
 
   it('should parse parameter positions', function () {
-    var spec = Keywordspec('i18n:1,2');
+    var spec = Keywordspec('i18n:2,3');
 
     assert('i18n' in spec);
-    assert.deepEqual(spec.i18n, [0, 1]);
+    assert.deepEqual(spec.i18n, [1, 2]);
+  });
+
+  it('should parse multiple keywords', function () {
+    var spec = Keywordspec('_, gettext, dgettext:2, dcgettext:2, ngettext:1,2, dngettext:2,3, pgettext:1c,2, dpgettext:2c,3');
+
+    assert('_' in spec);
+    assert.deepEqual(spec._, [0]);
+    assert('ngettext' in spec);
+    assert.deepEqual(spec.ngettext, [0,1]);
   });
 });
