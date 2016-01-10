1fc3bfae332491a41b197b4ee8551bedc51bed73
smhg <sam@strictlyphp.com>
2016-01-10 11:20:02 +0100

    add comment

--- src/keywordspec.js
+++ src/keywordspec.js
@@ -9,7 +9,7 @@ function addToSpec (spec, keyword) {
   var parts;
 
   while ((parts = specPattern.exec(keyword)) !== null) {
-
+    // non-digit positions are ignored (pgettext, dpgettext)
     spec[parts[1]] = [onlyDigits.test(parts[2]) ? indexify(parts[2]) : 0];
 
     if (parts[3] && onlyDigits.test(parts[3])) {
