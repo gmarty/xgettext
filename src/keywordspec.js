57f0626308e8d47c1c7377e626af405137ec362e
smhg <sam@strictlyphp.com>
2016-10-08 14:27:48 +0200

    parse context arguments keywordspec (see #56)

--- src/keywordspec.js
+++ src/keywordspec.js
@@ -1,26 +1,50 @@
-var specPattern = / *([^:,]+) *(?::([^, ]+)(?:,([^, ]+))?)? */g,
-  onlyDigits = /^\d+$/;
+var specPattern = / *([^:,]+) *(?::([^, ]+(?:,[^, ]+)*))? */g,
+  contextPattern = /^(\d)+c$/;
 
 function indexify (idx) {
   return parseInt(idx, 10) - 1;
 }
 
-function addToSpec (spec, keyword) {
+function addToSpec (spec, item) {
   var parts;
 
-  while ((parts = specPattern.exec(keyword)) !== null) {
-    // non-digit positions are ignored (pgettext, dpgettext)
-    spec[parts[1]] = [onlyDigits.test(parts[2]) ? indexify(parts[2]) : 0];
+  while ((parts = specPattern.exec(item)) !== null) {
+    var keyword = parts[1];
+    var positions = {position: 0};
 
-    if (parts[3] && onlyDigits.test(parts[3])) {
-      spec[parts[1]].push(indexify(parts[3]));
+    if (parts[2]) {
+      positions = parts[2]
+        .trim()
+        .split(/ ?, ?/)
+        .reduce(function (positions, position) {
+          var context = position.match(contextPattern);
+
+          if (context) {
+            positions.context = indexify(context[1]);
+          } else if (!('position' in positions)) {
+            positions.position = indexify(position);
+          } else {
+            positions.plural = indexify(position);
+          }
+
+          return positions;
+        }, {});
     }
+
+    // support <= 3.1 position array format
+    spec[keyword] = [positions.position];
+
+    if (positions.plural) {
+      spec[keyword].push(positions.plural);
+    }
+
+    Object.assign(spec[keyword], positions);
   }
 
   return spec;
 }
 
-module.exports = function createKeywordspec(spec) {
+module.exports = function createKeywordspec (spec) {
   if (typeof spec === 'string') {
     spec = [spec];
   }
--- test/keywordspec.js
+++ test/keywordspec.js
@@ -6,29 +6,40 @@ describe('Keywordspec', function () {
     var spec = Keywordspec('gettext');
 
     assert('gettext' in spec);
-    assert.deepEqual(spec.gettext, [0]);
+    assert.deepEqual(spec.gettext.slice(0), [0]);
   });
 
   it('should parse an array', function () {
     var spec = Keywordspec(['gettext']);
 
     assert('gettext' in spec);
-    assert.deepEqual(spec.gettext, [0]);
+    assert.deepEqual(spec.gettext.slice(0), [0]);
   });
 
   it('should parse parameter positions', function () {
     var spec = Keywordspec('i18n:2,3');
 
     assert('i18n' in spec);
-    assert.deepEqual(spec.i18n, [1, 2]);
+    assert.deepEqual(spec.i18n.slice(0), [1, 2]);
   });
 
   it('should parse multiple keywords', function () {
     var spec = Keywordspec('_, gettext, dgettext:2, dcgettext:2, ngettext:1,2, dngettext:2,3, pgettext:1c,2, dpgettext:2c,3');
 
     assert('_' in spec);
-    assert.deepEqual(spec._, [0]);
+    assert.deepEqual(spec._.slice(0), [0]);
     assert('ngettext' in spec);
-    assert.deepEqual(spec.ngettext, [0,1]);
+    assert.deepEqual(spec.ngettext.slice(0), [0, 1]);
+  });
+
+  it('should parse context position', function () {
+    var spec = Keywordspec('gettext, ngettext:1,2, pgettext:1c,2, npgettext:2c,3,4');
+
+    assert('pgettext' in spec);
+    assert.deepEqual(spec.pgettext.slice(0), [1]);
+    assert(spec.pgettext.context === 0);
+    assert('npgettext' in spec);
+    assert.deepEqual(spec.npgettext.slice(0), [2, 3]);
+    assert(spec.npgettext.context === 1);
   });
 });
